744036e8807c4ae7f0e0510a3a3ae3ffbafefa12
Narendras <n.syahrasyad.n@gmail.com>
2019-11-01 10:42:45 -0700

    Added ingredient controller

--- ingredient/src/routes/ingredient/ingredient.controller.ts
+++ ingredient/src/routes/ingredient/ingredient.controller.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from 'express';
+import { db } from '../../db';
 
 export const addIngredient = (req: Request, res: Response) => {
-    res.send('Not implemented');
+    const { userId } = req.params;
+    const { ingredientId } = req.body;
+
+    return db
+        .none(
+            `
+      insert into stored_ingredient
+      values ($1, $2);
+      `,
+            [userId, ingredientId]
+        )
+        .then(() => {
+            res.status(201).end();
+        })
+        .catch((error) => {
+            res.status(500).json({ error });
+        });
 };
