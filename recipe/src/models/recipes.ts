6a56784e9cfa9a7246e99ac88cde3f67bfcfde63
Noor Khan <noorkhanubc@gmail.com>
2019-11-08 17:10:12 -0800

    Feature/recipe (#37)
    * Boiler plate for recipe
    
    * adding a new line at the end of server.js
    
    * WIP feature/recipe
    
    * Basic endpoints for feature/recipe
    
    * Using common .env + Restructured Recipe
    
    * fixed reading values from dev.env
    
    * Removed JS files from src

---
+++ recipe/src/models/recipes.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+export interface IRecipes extends mongoose.Document {
+    recipe_name: string;
+    description: string;
+    instructions: string[];
+    ingredients: string[];
+}
+
+export const Recipes = new mongoose.Schema({
+    recipe_name: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    instructions: {
+        type: Array,
+    },
+    ingredients: {
+        type: Array,
+    },
+});
+
+export const RecipesModel = mongoose.model<IRecipes>('Recipes', Recipes);
