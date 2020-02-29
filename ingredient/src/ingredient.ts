67d4ef76d70509321dbe842667729f1cb3e8a4dc
Connor Fong <connor_fong@hotmail.com>
2020-02-29 13:56:27 -0800

    INGREDIENT: separated server functionality for testing

--- ingredient/src/ingredient.ts
+++ ingredient/src/ingredient.ts
@@ -1,17 +1,13 @@
 import { json, urlencoded } from 'body-parser';
 import * as express from 'express';
-
+import * as http from 'http';
 import { masterRouter } from './routes/_master-routes';
 
 const app = express();
-
-const PORT = process.env.PORT || 9000;
+const service = http.createServer(app);
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
 app.use('/', masterRouter);
 
-app.listen(PORT, () => {
-    console.log(`[Ingredient] listening on port ${PORT}`);
-});
+module.exports = service;
