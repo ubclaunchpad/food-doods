8b25ed420d41c60809b99002f65e8275301e1d78
Connor Fong <connor_fong@hotmail.com>
2020-03-02 15:46:09 -0800

    updated API - all stubs

--- ingredient/src/routes/_master-routes.ts
+++ ingredient/src/routes/_master-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { ingredientRouter } from './ingredient';
+import { searchRouter } from './search';
 import { userRouter } from './user';
 
 const masterRouter = Router();
 
 masterRouter.use('/ingredient', ingredientRouter);
+masterRouter.use('/search', searchRouter);
 masterRouter.use('/user', userRouter);
 masterRouter.get('/', (req, res) => {
     res.send('Ingredient Service');
