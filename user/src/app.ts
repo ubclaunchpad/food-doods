c350576d3eac3f4585d5ef1e3a50964ab01be3dd
Adrian Viquez <aviquez96@gmail.com>
2020-03-03 12:01:28 -0800

    Implemented preliminary layout for GET, POST, PATCH and DELETE routes for the User service

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,15 +1,32 @@
-import 'dotenv/config';
-import express, { Application } from 'express';
-import { router as userRouter } from './controllers/user';
+import express from 'express';
+import mongoose from 'mongoose';
 
-const PORT: string = process.env.PORT || '8000';
+// Import routes
+import bodyParser from 'body-parser';
+import { initializeUserRoutes } from './routes/routes';
 
-const app: Application = express();
-app.use(express.json());
+const PORT = process.env.USER_PORT || 8000;
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION || 'mongodb://localhost:27017/test';
+const app = express();
 
-/* connect routers */
-app.use('/', userRouter);
+// middle wares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log('This app is listening on the port ' + PORT + '!');
-});
+// initialize the User routes
+initializeUserRoutes(app);
+
+// Connect to DB
+mongoose
+    .connect(DB_CONNECTION_STRING, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('Connected to MongoDB!'));
+
+try {
+    app.listen(PORT);
+    console.log(`Server listening on port ${PORT}`);
+} catch (e) {
+    console.error(e);
+    throw e;
+}
+
+
