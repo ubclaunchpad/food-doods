729eaba5ee32075fae6f8d011edcb37ce0ab613f
Adrian Viquez <aviquez96@gmail.com>
2020-03-03 18:29:47 -0800

    Fixed strings

--- user/src/routes/routes.ts
+++ user/src/routes/routes.ts
@@ -6,21 +6,21 @@ export const initializeUserRoutes = (app: Application) => {
 
     /* gets a user */
     userRouter.get('/', async (req, res) => {
-        res.status(200).send("Working user get route")
+        res.status(200).send('Working user get route')
     });
 
     /* posts a user */
     userRouter.post('/', async (req, res) => {
-        res.status(200).send("Working user post route")
+        res.status(200).send('Working user post route')
     });
 
     /* deletes a user */
     userRouter.delete('/', async (req, res) => {
-        res.status(200).send("Working user delete route")
+        res.status(200).send('Working user delete route')
     });
 
     /* patches a user */
     userRouter.patch('/', async (req, res) => {
-        res.status(200).send("Working user patch route")
+        res.status(200).send('Working user patch route')
     });
 };
