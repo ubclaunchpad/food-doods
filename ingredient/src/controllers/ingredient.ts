4667632e5459fe21b87c75001a98af3383762bef
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-03-07 15:52:10 -0800

    Add stubs for /ingredient endpoints

---
+++ ingredient/src/controllers/ingredient.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+export const getIngredients = (req: Request, res: Response) => {
+    return res.status(200).json('stub');
+};
+
+export const postIngredient = (req: Request, res: Response) => {
+    return res.status(200).json('stub');
+};
+
+export const patchIngredient = (req: Request, res: Response) => {
+    return res.status(200).json('stub');
+};
+
+export const deleteIngredient = (req: Request, res: Response) => {
+    return res.status(200).json('stub');
+};
---
+++ ingredient/src/routes/ingredient.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import * as controller from '../controllers/ingredient';
+
+/*
+ * Responsible for managing general ingredient data within the application
+ */
+const ingredientRouter = Router();
+
+ingredientRouter.get('/:id', controller.getIngredients);
+
+ingredientRouter.post('/', controller.postIngredient);
+
+ingredientRouter.patch('/:id', controller.patchIngredient);
+
+ingredientRouter.delete('/:id', controller.deleteIngredient);
+
+export { ingredientRouter };
