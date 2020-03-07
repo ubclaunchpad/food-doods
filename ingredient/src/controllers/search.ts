3f88d24479e21f1ee0ba81ae62a882f408743b54
Andrew <44531733+andrewzulaybar@users.noreply.github.com>
2020-03-07 14:00:22 -0800

    Move searchIngredient from ingredient.ts into search.ts (#116)

--- ingredient/src/controllers/search.ts
+++ ingredient/src/controllers/search.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { db } from '../db/connection';
 
 export const searchIngredient = (req: Request, res: Response) => {
     // query: ingredient in database by name
@@ -10,5 +11,16 @@ export const searchIngredient = (req: Request, res: Response) => {
         });
     }
 
-    return res.send('stub');
+    return db
+        .any('SELECT * FROM ingredient WHERE LOWER(name) LIKE LOWER(%$1%)', [query])
+        .then((ingredients: any[]) => {
+            if (ingredients.length > 0) {
+                return res.status(200).json({ ingredients });
+            } else {
+                return res.status(404).json({ error: 'No matching ingredients were found' });
+            }
+        })
+        .catch((error: any) => {
+            return res.status(500).json({ error });
+        });
 };
