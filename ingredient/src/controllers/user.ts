161e9e4a398c217fbce32a6334bb255e1fa9d37b
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-03-14 14:23:16 -0700

    Fix DELETE /user controller

--- ingredient/src/controllers/user.ts
+++ ingredient/src/controllers/user.ts
@@ -12,7 +12,14 @@ export const addUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     return db
-        .none('DELETE FROM user_map WHERE external_id = $1', [id])
-        .then(() => res.status(204).json({ message: 'Successfully deleted user!' }))
-        .catch((error: Error) => res.status(404).json({ error }));
+        .any('SELECT * FROM user_map WHERE id = $1', [id])
+        .then((user) => {
+            if (user.length > 0) {
+                return db.none('DELETE FROM user_map WHERE id = $1', [id]);
+            } else {
+                throw new Error('User does not exist in database');
+            }
+        })
+        .then(() => res.status(200).json({ message: 'Successfully deleted user!' }))
+        .catch((error: Error) => res.status(404).send({ error: error.message }));
 };
