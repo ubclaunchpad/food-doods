33e3b272dd33bca39cb8188403e083d0bbfe0f4d
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-21 12:46:10 -0700

    Implement get quantity

--- ingredient/__tests__/util/parser.test.ts
+++ ingredient/__tests__/util/parser.test.ts
@@ -2,16 +2,16 @@ import { getQuantity, parse } from '../../src/util/parser';
 
 describe('Parser', () => {
     describe('Parse Quantity', () => {
-        const cases: Array<[string, number, string]> = [
-            ['6 eggs', 6, 'Should parse a simple number at start of ingredient'],
-            ['hello', null, "Should return null if ingredient doesn't start with a number"],
-            ['1/4 teaspoon salt', 0.25, 'Should parse fractions'],
-            ['1 1/4 cups buttermilk', 1.25, 'Should parse mixed numbers'],
-            ['2 (6 ounce) cans tomatoes', 6, 'Should prioritize quantities inside brackets'],
+        const cases: Array<[string, [number, string], string]> = [
+            ['6 eggs', [6, 'eggs'], 'Should parse a simple number at start of ingredient'],
+            ['hello', [null, 'hello'], "Should return null if ingredient doesn't start with a number"],
+            ['1/4 teaspoon salt', [0.25, 'teaspoon salt'], 'Should parse fractions'],
+            ['1 1/4 cups buttermilk', [1.25, 'cups buttermilk'], 'Should parse mixed numbers'],
+            ['2 (6 ounce) cans tomatoes', [6, 'ounce) cans tomatoes'], 'Should prioritize quantities inside brackets'],
         ];
         cases.forEach(([ingredient, quantity, message]) => {
             it(message, () => {
-                expect(getQuantity(ingredient)[0]).toEqual(quantity);
+                expect(getQuantity(ingredient)).toEqual(quantity);
             });
         });
     });
--- ingredient/src/util/parser.ts
+++ ingredient/src/util/parser.ts
@@ -1,5 +1,8 @@
 import { IRecipe, IRecipeIngredient, UnitCategory } from '../types/_master-types';
 
+const bracketContentsRegex = /\((.*?)\)/;
+const bracketRegex = /[()]/;
+
 const volumeUnits = ['cup', 'teaspoon', 'tablespoon', 'ounce', 'pinch'];
 const weightUnits = ['gram', 'pound'];
 
@@ -31,7 +34,56 @@ export function parse(recipe: IRecipe): IRecipeIngredient[] {
 }
 
 export function getQuantity(ingredient: string): [number, string] {
-    return [0, ingredient];
+    const words = ingredient.split(' ');
+    let maybeNum = words[0];
+    let cursor = 0;
+
+    // Check for any brackets that start with a number
+    const match = bracketContentsRegex.exec(ingredient);
+    if (match) {
+        const maybePrioritize = match[0].replace(bracketRegex, '');
+        const startsWithNumber = !isNaN(parseFloat(maybePrioritize));
+        if (maybePrioritize.split(' ').length === 2 && startsWithNumber) {
+            maybeNum = maybePrioritize.split(' ')[0];
+            cursor = match.index;
+        }
+    }
+
+    // Check for fractions
+    if (maybeNum.includes('/')) {
+        const fraction = getFraction(maybeNum);
+        if (!fraction) {
+            return [null, ingredient];
+        }
+        cursor += maybeNum.length;
+        return [fraction, ingredient.slice(cursor + 1).trimLeft()];
+    }
+
+    let quantity = parseFloat(maybeNum);
+    if (isNaN(quantity)) {
+        return [null, ingredient];
+    }
+    cursor += maybeNum.length;
+
+    // Check for additional fractions
+    const maybeMixed = words[1];
+    if (maybeMixed.includes('/')) {
+        const fraction = getFraction(maybeMixed);
+        if (!fraction) {
+            return [null, ingredient];
+        }
+        quantity += fraction;
+        cursor += maybeMixed.length;
+    }
+    return [quantity, ingredient.slice(cursor + 1).trimLeft()];
+}
+
+function getFraction(maybeFraction: string): number {
+    const [numer, denom] = maybeFraction.split('/').map(parseFloat);
+    if (isNaN(numer) || isNaN(denom)) {
+        return null;
+    }
+    return numer / denom;
 }
 
 function getUnitCategory(ingredient: string): [UnitCategory, string] {
