3c5f861a8f3f554864d6527aafc4c28e24d39edd
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-20 17:18:03 -0700

    Add tests

--- ingredient/__tests__/util/parser.test.ts
+++ ingredient/__tests__/util/parser.test.ts
@@ -1,9 +1,18 @@
+import { getQuantity, parse } from '../../src/util/parser';
+
 describe('Parser', () => {
     describe('Parse Quantity', () => {
-        it.todo('Should parse a simple number at start of ingredient', () => {});
-        it.todo("Should return null if ingredient doesn't start with a number", () => {});
-        it.todo('Should parse fractions', () => {});
-        it.todo('Should parse mixed numbers', () => {});
-        it.todo('Should prioritize quantities inside brackets', () => {});
+        const cases: Array<[string, number, string]> = [
+            ['6 eggs', 6, 'Should parse a simple number at start of ingredient'],
+            ['hello', null, "Should return null if ingredient doesn't start with a number"],
+            ['1/4 teaspoon salt', 0.25, 'Should parse fractions'],
+            ['1 1/4 cups buttermilk', 1.25, 'Should parse mixed numbers'],
+            ['2 (6 ounce) cans tomatoes', 6, 'Should prioritize quantities inside brackets'],
+        ];
+        cases.forEach(([ingredient, quantity, message]) => {
+            it(message, () => {
+                expect(getQuantity(ingredient)[0]).toEqual(quantity);
+            });
+        });
     });
 });
