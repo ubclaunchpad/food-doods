1289e7354580a373def848709e32cc3849b477c8
Adrian Viquez <aviquez96@gmail.com>
2020-03-21 12:29:24 -0700

    WIP

--- suggestion/src/routes/suggestion.ts
+++ suggestion/src/routes/suggestion.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { suggestRecipes } from '../controller/suggestRecipe';
 
 const router = express.Router();
@@ -20,6 +22,21 @@ router.post('/', (req, res) => {
 
     // return as a json object with key "hashes"
     res.status(200).json({ hashes: recipeHashes });
+});
+
+router.post('/:userId', (req, res) => {
+    // get all ingredients
+    // const ingredients = GET Ingredients of a User by User Id
+    const object = JSON.parse(readFileSync(resolve('mocks/sampleIngredients.json')).toString());
+    // hash the ingredients
+    // get all the recipes
+    // const recipes = GET All the Recipes
+    // compare hashed ingredients with recipes
+    const recipeNumber = 3;
+    const recipes = JSON.parse(readFileSync(resolve('mocks/hashes.json')).toString());
+    const suggestedRecipes = suggestRecipes(object.ingredients, recipeNumber, recipes);
+    console.log(suggestedRecipes);
+    // return recipe id's
 });
 
 module.exports = router;
