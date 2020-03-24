b6999c2703269f2864a92d3a04ea0527c384577c
Andrew <44531733+andrewzulaybar@users.noreply.github.com>
2020-03-23 17:06:19 -0700

    Add tests for user service (#135)
    * Refactor getUser controller
    
    * Move findUser and retrieveUsers into user.ts
    
    * Alphebatize dev dependencies and add test script
    
    * Add specs for getUserToken and getUserAttributes
    
    * Add first test for getUserToken
    
    * Add tests for getUserToken
    
    * Add tests for getUserAttributes
    
    * Add specs for POST /user tests
    
    * Add first test for createUser
    
    * Add tests against missing properties
    
    * Finish up rest of tests for postUser
    
    * Minor fixes
    
    * Add test for duplicate location
    
    * Small fix
    
    * Add tests for verifyUser
    
    * Create db folder

--- user/jest.config.js
+++ user/jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+    testPathIgnorePatterns: ['<rootDir>/build'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
     transform: {
         '.(ts|tsx)': 'ts-jest',
     },
+    testTimeout: 10000,
 };
---
+++ user/src/util/errors/AuthorizationError.ts
@@ -0,0 +1,9 @@
+class AuthorizationError extends Error {
+    constructor(public message: string) {
+        super();
+        this.name = 'AuthorizationError';
+        this.message = message;
+    }
+}
+
+export { AuthorizationError };
