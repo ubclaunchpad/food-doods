3bdd33e267bd4645606581f3886345ab8b00594a
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-03-28 12:39:38 -0700

    Add GET /recipes call to suggestion service

--- suggestion/src/routes/suggestion.ts
+++ suggestion/src/routes/suggestion.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { suggestRecipes } from '../controller/suggestRecipe';
+import { getRecipes, hashRecipes } from '../util/recipe';
 
 const router = express.Router();
 
@@ -25,18 +26,19 @@ router.post('/', (req, res) => {
 });
 
 // POST /suggestion/:userId
-router.post('/:userId', (req, res) => {
+router.post('/:userId', async (req: Request, res: Response) => {
     // get all ingredients
     // const ingredients = GET Ingredients of a User by User Id; currently mocked
     const object = JSON.parse(readFileSync(resolve('mocks/sampleIngredients.json')).toString());
-    // const recipes = GET All the Recipes; currently mocked
-    const recipes = JSON.parse(readFileSync(resolve('mocks/hashes.json')).toString());
+
+    const setOfRecipes: Set<object> = await getRecipes();
+    const recipes: string[] = hashRecipes(setOfRecipes, object.ingredients);
     const recipeNumber = 0.3;
     // return recipe id's
     // the suggestRecipes controller is currently  has a TODO ticket that's
     // meant to return the recipes instead of the hashes
-    const suggestions = suggestRecipes(object.ingredients, recipeNumber, recipes);
-    res.status(200).json({ recipes: suggestions });
+    const suggestions: number[] = suggestRecipes(object.ingredients, recipeNumber, recipes);
+    return res.status(200).json({ recipes: suggestions });
 });
 
 module.exports = router;
