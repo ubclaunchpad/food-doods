f0aa27f2977fc3181ad3407e87aaa38aa1956b12
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-27 20:31:04 -0700

    Fetch ingredients from ingredient service

--- suggestion/src/routes/suggestion.ts
+++ suggestion/src/routes/suggestion.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as express from 'express';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
@@ -25,17 +26,15 @@ router.post('/', (req, res) => {
 });
 
 // POST /suggestion/:userId
-router.post('/:userId', (req, res) => {
-    // get all ingredients
-    // const ingredients = GET Ingredients of a User by User Id; currently mocked
-    const object = JSON.parse(readFileSync(resolve('mocks/sampleIngredients.json')).toString());
+router.post('/:userId', async (req, res) => {
+    const { ingredients } = await axios.get(`localhost:${process.env.INGREDIENT_PORT}/user/${req.params.userId}`);
     // const recipes = GET All the Recipes; currently mocked
     const recipes = JSON.parse(readFileSync(resolve('mocks/hashes.json')).toString());
     const recipeNumber = 0.3;
     // return recipe id's
     // the suggestRecipes controller is currently  has a TODO ticket that's
     // meant to return the recipes instead of the hashes
-    const suggestions = suggestRecipes(object.ingredients, recipeNumber, recipes);
+    const suggestions = suggestRecipes(ingredients, recipeNumber, recipes);
     res.status(200).json({ recipes: suggestions });
 });
 
