dffe364a86a46851c539814fb317b0098b91fd92
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-03-28 12:38:37 -0700

    Add types and response codes to recipe controllers

--- recipe/src/routes/routes.ts
+++ recipe/src/routes/routes.ts
@@ -1,4 +1,5 @@
-import { Application, Router } from 'express';
+import { Application, Router, Request, Response } from 'express';
+import { Document } from 'mongoose';
 import { IRecipes, RecipesModel } from '../models/recipes';
 import { UserRecipesModel } from '../models/userRecipes';
 const recipeScraper = require('recipe-scraper');
@@ -8,78 +9,76 @@ export const initializeRecipeRoutes = (app: Application) => {
     app.use('/recipe', recipeRouter);
 
     /* create a recipe */
-    recipeRouter.post('/', async (req, res) => {
+    recipeRouter.post('/', async (req: Request, res: Response) => {
         const recipe = new RecipesModel(req.body);
         try {
-            await recipe.save().then((item) => res.send(item));
+            await recipe.save().then((recipe: Document) => res.status(201).json({ recipe }));
         } catch (e) {
             console.error(e);
-            res.status(500);
-            res.json('Could not create the recipe');
+            res.status(500).json('Could not create the recipe');
         }
     });
 
     /* get all the recipes of the user with user_id */
-    recipeRouter.get('/user/:user_id', async (req, res) => {
+    recipeRouter.get('/user/:user_id', async (req: Request, res: Response) => {
         try {
             const user = await UserRecipesModel.findById(req.params.user_id);
             if (!user) {
                 return res.status(400).send(`No user exists with id ${req.params.user_id}`);
             }
             if (!user.recipe_ids || user.recipe_ids.length === 0) {
-                res.json('No recipes associated with this user');
+                res.status(200).send('No recipes associated with this user');
             } else {
                 RecipesModel.find(
                     {
                         _id: { $in: user.recipe_ids },
                     },
-                    (err, docs) => {
-                        res.json(docs);
+                    (err: Error, recipes: Document) => {
+                        if (err) {
+                            throw err;
+                        }
+                        res.status(200).json({ recipes });
                     }
                 );
             }
         } catch (e) {
             console.error(e);
-            res.status(500);
-            res.json('Could not fetch the recipes for the user');
+            res.status(500).send('Could not fetch the recipes for the user');
         }
     });
 
     /* create a user */
-    recipeRouter.post('/user', async (req, res) => {
+    recipeRouter.post('/user', async (req: Request, res: Response) => {
         const user = new UserRecipesModel(req.body);
         try {
-            await user.save().then((usr) => res.send(usr));
+            await user.save().then((user: Document) => res.status(204).send(user.toObject()));
         } catch (e) {
             console.error(e);
-            res.status(500);
-            res.json('Could not create the user');
+            res.status(500).send('Could not create the user');
         }
     });
 
     /* remove a user */
-    recipeRouter.delete('/user/:user_id', async (req, res) => {
+    recipeRouter.delete('/user/:user_id', async (req: Request, res: Response) => {
         try {
-            await UserRecipesModel.findByIdAndDelete(req.params.user_id).then((usr) =>
-                res.send(`${usr} \n \n Removed from DB`)
-            );
+            await UserRecipesModel.findByIdAndDelete(req.params.user_id).then((user: Document) => {
+                const username: string = user.get('username');
+                res.status(200).send(`${username} \n \n Removed from DB`);
+            });
         } catch (e) {
             console.error(e);
-            res.status(500);
-            res.json('Could not delete the user');
+            res.status(500).send('Could not delete the user');
         }
     });
 
     /* gets a recipe associated to an id*/
-    recipeRouter.get('/recipe/:recipe_id', async (req, res) => {
+    recipeRouter.get('/recipe/:recipe_id', async (req: Request, res: Response) => {
         try {
             const recipe = await RecipesModel.findById(req.params.recipe_id);
             if (recipe === null) {
                 res.status(404).send(`No such recipe found with query ${req.query}`);
-            }
-            else {
-                res.status(200);
-                await res.json(recipe);
+            } else {
+                res.status(200).json({ recipe });
             }
         } catch (e) {
             res.status(500).send(`Could not get the recipe with id ${req.params.id} for ${e.message}`);
@@ -87,7 +86,7 @@ export const initializeRecipeRoutes = (app: Application) => {
     });
 
     /* gets 'count' number of recipes starting at position 'index'*/
-    recipeRouter.get('/recipe', async (req, res) => {
+    recipeRouter.get('/recipe', async (req: Request, res: Response) => {
         try {
             let { count, index } = req.query;
             count = parseInt(count, 10);
@@ -95,25 +94,28 @@ export const initializeRecipeRoutes = (app: Application) => {
             const recipes = await RecipesModel.find()
                 .skip(index)
                 .limit(count);
+            recipes.forEach((recipe: Document) => recipe.toObject());
             if (recipes === null || recipes.length === 0) {
                 res.status(404).send(`No such recipes found with query ${req.query}`);
             } else {
-                res.status(200);
-                await res.json(recipes);
+                res.status(200).json({ recipes });
             }
         } catch (e) {
             res.status(500).send(`Could not get the recipes with query ${req.query} for ${e.message}`);
         }
     });
 
-    recipeRouter.post('/recipe-scraper', async (req, res) => {
+    recipeRouter.post('/recipe-scraper', async (req: Request, res: Response) => {
         if (!req.body.recipeUrl) {
             return res.status(400).send('Please pass recipeUrl in the body');
         }
         recipeScraper(req.body.recipeUrl)
             .then((recipe: IRecipes) => {
                 const postRecipe = new RecipesModel(recipe);
-                return postRecipe.save().then((r) => res.send(r), () => res.status(500).send('Could not save to DB'));
+                return postRecipe
+                    .save()
+                    .then((r: Response) => res.send(r))
+                    .catch(() => res.status(500).send('Could not save to DB'));
             })
             .catch(() => {
                 return res.status(404).send('No recipe found on the page');
