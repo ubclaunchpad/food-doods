4bbd016580c89d010b85461e8a843e1a85672374
Alan Yan <yanalan7@gmail.com>
2020-03-30 19:05:05 -0700

    detail recipe view with linkage to clicked recipe

--- iOS/Food-Doods-Prototype/Food-Doods-Prototype/Views/RecipeDetailedView.swift
+++ iOS/Food-Doods-Prototype/Food-Doods-Prototype/Views/RecipeDetailedView.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import AlanYanHelpers
 
 class RecipeInfoCell: UIView {
     var imageView: UIImageView = {
@@ -64,7 +64,29 @@ class RecipeDetailedView: UIView {
     var viewModel: Recipe! {
         didSet {
             recipeImage.image = viewModel.image
-            recipeLabel.text = viewModel.name
+            bottomView.recipeLabel.text = viewModel.name
+            
+            bottomView.stackView.addArrangedSubview(RecipeImageTextView(image: UIImage(systemName: "timer"), size: 16, title: "\(Int(viewModel.time.rounded())) minutes"))
+            bottomView.stackView.addArrangedSubview(RecipeImageTextView(image: UIImage(systemName: "circle"), size: 16, title: viewModel.difficulty))
+            bottomView.stackView.addArrangedSubview(RecipeImageTextView(image: UIImage(systemName: "person.3"), size: 16, title: "2 People"))
+            
+            var ingredOwned: String = ""
+            
+            for i in viewModel.ingredientsOwned {
+                ingredOwned.append(i.name)
+                ingredOwned.append("\n")
+            }
+            
+            bottomView.ingredientsOwnedLabel.text = ingredOwned
+            
+            var ingredNotOwned: String = ""
+            
+            for i in viewModel.ingredientsNeeded {
+                ingredNotOwned.append(i.name)
+                ingredNotOwned.append("\n")
+            }
+            
+            bottomView.ingredientsMissingLabel.text = ingredNotOwned
         }
     }
     
@@ -76,14 +98,7 @@ class RecipeDetailedView: UIView {
         imageView.clipsToBounds = true
         return imageView
     }()
-    var bottomView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .white
-        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        view.layer.cornerRadius = 20
-        return view
-    }()
+    var bottomView = RecipeScrollDescriptionView()
     var recipeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -104,7 +119,6 @@ class RecipeDetailedView: UIView {
     func setupView() {
         addSubview(recipeImage)
         addSubview(bottomView)
-        bottomView.addSubview(recipeLabel)
         setupConstraints()
     }
    
@@ -115,14 +129,159 @@ class RecipeDetailedView: UIView {
         recipeImage.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
         recipeImage.bottomAnchor.constraint(equalTo: centerYAnchor, constant: -100).isActive = true
         
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
         bottomView.topAnchor.constraint(equalTo: recipeImage.bottomAnchor, constant: -20).isActive = true
-        bottomView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        bottomView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        bottomView.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
         bottomView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         
-        recipeLabel.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 20).isActive = true
-        recipeLabel.leftAnchor.constraint(equalTo: bottomView.leftAnchor, constant: 20).isActive = true
-        recipeLabel.rightAnchor.constraint(equalTo: bottomView.rightAnchor, constant: -20).isActive = true
         
     }
+}
+
+
+class RecipeScrollDescriptionView: UIScrollView {
+    
+    var recipeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Bold", size: 21)
+        return label
+    }()
+    
+    var selectionSegmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl()
+        control.insertSegment(withTitle: "Ingredients", at: 0, animated: true)
+        control.insertSegment(withTitle: "Preparation", at: 1, animated: true)
+        control.insertSegment(withTitle: "Reviews", at: 2, animated: true)
+        return control
+    }()
+    
+    var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    var ingredientLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Ingredients"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Bold", size: 18)
+        return label
+    }()
+    
+    var youHaveLabel: UILabel = {
+        let label = UILabel()
+        label.text = "You have:"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Book", size: 18)
+        return label
+    }()
+    
+    var ingredientsOwnedLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "14 oz new york strip steak (400g), or other steak of good quality \n2 teaspoons kosher salt \n2 tablespoons coarsely ground black pepper \n1 tablespoon vegetable oil"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Book", size: 18)
+        return label
+    }()
+    
+    var youNeedLabel: UILabel = {
+        let label = UILabel()
+        label.text = "You're missing:"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Book", size: 18)
+        return label
+    }()
+    
+    var ingredientsMissingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "2 tablespoons butter, divided \n1 cup cream (240 mL) \n1/3 cup brandy or cognac (80 mL) \n1 tablespoon dijon mustard"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont(name: "CircularStd-Book", size: 18)
+        return label
+    }()
+    override init(frame: CGRect) {
+         super.init(frame: frame)
+         setupView()
+    }
+    override class var requiresConstraintBasedLayout: Bool {
+        return true
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
+        backgroundColor = .white
+        addCorners(radius: 13, corners: [.layerMaxXMinYCorner, .layerMinXMinYCorner]).done()
+        
+        recipeLabel.setSuperview(self).addTop(constant: 25).addLeft(constant: 20).addRight(constant: -20).done()
+                
+        stackView.setSuperview(self).addLeft().addRight().addTop(anchor: recipeLabel.bottomAnchor, constant: 23).addHeight(withConstant: 30).done()
+        
+        selectionSegmentedControl.setSuperview(self).addTop(anchor: stackView.bottomAnchor, constant: 53).addLeft().addRight().addWidth(withConstant: UIScreen.main.bounds.width).done()
+
+        ingredientLabel.setSuperview(self).addLeft(constant: 20).addTop(anchor: selectionSegmentedControl.bottomAnchor, constant: 22).done()
+        
+        youHaveLabel.setSuperview(self).addLeft(constant: 20).addTop(anchor: ingredientLabel.bottomAnchor, constant: 16).done()
+        
+        ingredientsOwnedLabel.setSuperview(self).addLeft(constant: 37).addTop(anchor: youHaveLabel.bottomAnchor, constant: 17).addRight(constant: -37).done()
+        
+        youNeedLabel.setSuperview(self).addLeft(constant: 20).addTop(anchor: ingredientsOwnedLabel.bottomAnchor, constant: 16).done()
+        
+        ingredientsMissingLabel.setSuperview(self).addLeft(constant: 37).addTop(anchor: youNeedLabel.bottomAnchor, constant: 17).addRight(constant: -37).addBottom().done()
+     }
+}
+
+
+class RecipeImageTextView: UIView {
+    var imageView = ContentFitImageView()
+    var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        return label
+    }()
+    let view = UIView()
+    convenience init(image: UIImage?, size: CGFloat, title: String) {
+        self.init()
+        imageView.image = image
+        label.font = UIFont(name: "CircularStd-Book", size: size)
+        label.text = title
+        view.addWidth(withConstant: CGFloat(label.text!.count * 10)).done()
+        setupView()
+    }
+    
+    override init(frame: CGRect) {
+         super.init(frame: frame)
+         setupView()
+    }
+    override class var requiresConstraintBasedLayout: Bool {
+        return true
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+
+        view.setSuperview(self).addCenterX().addTop().addBottom().done()
+        imageView.setSuperview(view).addLeft(constant: 0).addTop().addBottom().addWidth(anchor: heightAnchor).done()
+        label.setSuperview(view).addLeft(anchor: imageView.rightAnchor, constant: 4).addCenterY().done()
+     }
 }
