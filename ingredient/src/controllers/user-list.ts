f5ad198ab422b2689c659cce58df762dc99a3311
Andrew <44531733+andrewzulaybar@users.noreply.github.com>
2020-04-04 16:42:10 -0700

    Change all /user/ingredient/:id endpoints to use MongoDB id (#155)
    * Change GET /user/ingredient/:id to use MongoDB id
    
    * Use MongoDB for all /user/ingredient/ endpoints

--- ingredient/src/controllers/user-list.ts
+++ ingredient/src/controllers/user-list.ts
@@ -1,52 +1,63 @@
 import { Request, Response } from 'express';
 import { db } from '../db/connection';
 
-export const getIngredients = (req: Request, res: Response): Promise<Response> => {
-    const { id: userId } = req.params;
+export const getIngredients = async (req: Request, res: Response): Promise<any> => {
+    const { id } = req.params;
 
     return db
-        .any(
-            'SELECT * \
-            FROM user_ingredient \
-            INNER JOIN ingredient ON user_ingredient.ingredient_id=ingredient.id \
-            WHERE user_id = $1',
-            [userId]
+        .one('SELECT * FROM user_map WHERE external_id = $1', [id])
+        .then((user: any) =>
+            db.any(
+                'SELECT I.id as id, I.name as name, UI.quantity as quantity, U.name as unit \
+                FROM user_ingredient UI \
+                INNER JOIN ingredient I ON UI.ingredient_id=I.id \
+                INNER JOIN unit_category U ON I.unit_category=U.id \
+                WHERE UI.user_id = $1',
+                [user.id]
+            )
         )
-        .then((ingredients: any[]) => res.status(200).json({ userId, ingredients }))
-        .catch((error: Error) => res.status(404).json({ error }));
+        .then((ingredients: any[]) => res.status(200).json({ id, ingredients }))
+        .catch(() => res.status(404).json({ error: `No users found with the id: ${id}` }));
 };
 
-export const addIngredient = (req: Request, res: Response): Promise<Response> => {
+export const addIngredient = async (req: Request, res: Response): Promise<Response> => {
     const { id: userId } = req.params;
     const { id, quantity } = req.body;
 
     return db
-        .none('INSERT INTO user_ingredient VALUES ($1, $2, $3)', [userId, id, quantity])
+        .one('SELECT * FROM user_map WHERE external_id = $1', [userId])
+        .then((user: any) => db.none('INSERT INTO user_ingredient VALUES ($1, $2, $3)', [user.id, id, quantity]))
         .then(() => res.status(201).json({ message: "Successfully added ingredient to user's list!" }))
         .catch((error: Error) => res.status(500).json({ error }));
 };
 
-export const updateIngredient = (req: Request, res: Response): Promise<Response> => {
+export const updateIngredient = async (req: Request, res: Response): Promise<Response> => {
     const { id: userId } = req.params;
     const { id, quantity } = req.body;
 
     return db
-        .none(
-            'UPDATE user_ingredient \
-            SET quantity = $1 \
-            WHERE user_id = $2 AND ingredient_id = $3',
-            [quantity, userId, id]
+        .one('SELECT * FROM user_map WHERE external_id = $1', [userId])
+        .then((user: any) =>
+            db.none(
+                'UPDATE user_ingredient \
+                SET quantity = $1 \
+                WHERE user_id = $2 AND ingredient_id = $3',
+                [quantity, user.id, id]
+            )
         )
         .then(() => res.status(204).json({ message: 'Successfully updated ingredient quantity!' }))
         .catch((error: Error) => res.status(500).json({ error }));
 };
 
-export const deleteIngredient = (req: Request, res: Response): Promise<Response> => {
+export const deleteIngredient = async (req: Request, res: Response): Promise<Response> => {
     const { id: userId } = req.params;
     const { id } = req.body;
 
     return db
-        .none('DELETE FROM user_ingredient WHERE user_id = $1 AND ingredient_id = $2', [userId, id])
+        .one('SELECT * FROM user_map WHERE external_id = $1', [userId])
+        .then((user: any) =>
+            db.none('DELETE FROM user_ingredient WHERE user_id = $1 AND ingredient_id = $2', [user.id, id])
+        )
         .then(() => res.status(204).json({ message: "Successfully deleted ingredient from user's list" }))
         .catch((error) => res.status(404).json({ error }));
 };
--- ingredient/src/routes/user-list.ts
+++ ingredient/src/routes/user-list.ts
@@ -13,7 +13,7 @@ const userListRouter = Router();
 /*
  * GET /user/ingredient/:id
  *
- * retrieves the list of ingredients owned by a user
+ * retrieves a user's list of ingredients
  */
 userListRouter.get('/:id', controller.getIngredients);
 
